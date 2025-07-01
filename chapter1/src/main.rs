8fffc8bb26822ca83f10621afd93769c77ecc6c2
W. J. van der Laan <laanwj@protonmail.com>
2025-07-01 05:05:39 +0000

    convert to proper tests

--- chapter1/src/main.rs
+++ chapter1/src/main.rs
@@ -65,29 +65,6 @@ fn rand_key_mod_p(p: UBig) -> UBig {
 }
 
 fn main() {
-    /*
-    let a: UBig = UBig::parse_str_radix("a4b48d82c05eb1b29f73f4875e9839b97a971eea1c53e96c4658942f57b8dd8a", 16);
-    let b: UBig = UBig::parse_str_radix("63fe5ad54fb61ed1f6e2713feddeac53c1e064417e80be452c186237601312d0", 16);
-    let c: UBig = UBig::parse_str_radix("fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f", 16);
-
-    println!("0x{:x}", add_mod(a, b, c));
-    println!("0x{:x}", mul_mod(a, b, c));
-    println!("0x{:x}", add_mod(c.sub(1.as_()), c.sub(1.as_()), c));
-    println!("0x{:x}", mul_mod(c.sub(1.as_()), c.sub(1.as_()), c));
-    println!("exp 1 0x{:x}", exp_mod(a, 1.as_(), c));
-    println!("exp 2 0x{:x}", exp_mod(a, 2.as_(), c));
-    println!("exp 4 0x{:x}", exp_mod(a, 4.as_(), c));
-    println!("exp 5 0x{:x}", exp_mod(a, 5.as_(), c));
-    println!("exp 8 0x{:x}", exp_mod(a, 8.as_(), c));
-    println!("exp 0x12345678 0x{:x}", exp_mod(a, 0x12345678.as_(), c));
-
-    let p = c;
-    for b in 1..10 {
-        let inv = mod_inv(b.as_(), p.as_());
-        println!("modinv({}) {}*{}={} mod p", p, b, inv, mul_mod(b.as_::<UBig>(), inv, p.as_::<UBig>()));
-    }
-    */
-
     /* (domain parameters) */
     let p: UBig = UBig::parse_str_radix("eacb15fa75b90bbbe13663a539814e3318ec6b21cc5d51c1a8182484ffa90edf", 16);
     let g: UBig = UBig::parse_str_radix("937a57cdc95f6717f6d90b4286568c2c9aca750bfd1069b00cbf28abc17ba191", 16);
@@ -102,4 +79,35 @@ fn main() {
     let y2: UBig = exp_mod(g, x2, p);
     println!("x2 {:x}", x2);
     println!("y2 {:x}", y2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tests() {
+        let a: UBig = UBig::parse_str_radix("a4b48d82c05eb1b29f73f4875e9839b97a971eea1c53e96c4658942f57b8dd8a", 16);
+        let b: UBig = UBig::parse_str_radix("63fe5ad54fb61ed1f6e2713feddeac53c1e064417e80be452c186237601312d0", 16);
+        let c: UBig = UBig::parse_str_radix("fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f", 16);
+
+        assert_eq!(add_mod(a, b, c), UBig::parse_str_radix("8b2e8581014d084965665c74c76e60d3c77832b9ad4a7b17270f667b7cbf42b", 16));
+        assert_eq!(mul_mod(a, b, c), UBig::parse_str_radix("d464555ce28a5038f079c5deb138d690ce17b5494e74cfd476ce90d8111a977f", 16));
+        assert_eq!(add_mod(c.sub(UBig::ONE), c.sub(UBig::ONE), c), UBig::parse_str_radix("fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2d", 16));
+        assert_eq!(exp_mod(a, 1.as_(), c), UBig::parse_str_radix("a4b48d82c05eb1b29f73f4875e9839b97a971eea1c53e96c4658942f57b8dd8a", 16));
+        assert_eq!(exp_mod(a, 2.as_(), c), UBig::parse_str_radix("df8260402e69f79604fed563aa05a46ff0154fe44bf61395b47389fca7cd2456", 16));
+        assert_eq!(exp_mod(a, 4.as_(), c), UBig::parse_str_radix("d7f85719c86c38a423b316e96edd305684a6945cfe09962755c5e710ecfe4c3e", 16));
+        assert_eq!(exp_mod(a, 5.as_(), c), UBig::parse_str_radix("f7bebff2021a7c03b043fe20eaae900af6a46eec64feb01ea9143debb9615a61", 16));
+        assert_eq!(exp_mod(a, 8.as_(), c), UBig::parse_str_radix("26e4819834c6d91cdfb5d2b58205bcab017629be932d83bce74a71f5fea5fe99", 16));
+        assert_eq!(exp_mod(a, 0x12345678.as_(), c), UBig::parse_str_radix("8ac2769dd082094c4c4366047e17615673e9a8978da0f8ed9a53459a2f2c3294", 16));
+
+        let p = c;
+        for b in 1u32..10u32 {
+            let b_big = UBig::from(b);
+            let inv = mod_inv(b_big, p);
+            let mult = mul_mod(b_big, inv, p);
+            assert_eq!(mult, UBig::ONE);
+            // println!("modinv({}) {}*{}={} mod p", p, b, inv, mult);
+        }
+    }
 }
